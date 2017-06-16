0ff0abcbe67f26dc2a755fc8497d5e7da01b8422
Facundo Olano <facundo.olano@gmail.com>
2017-06-16 17:29:18 -0300

    use a separate ets instead of server state for subscribers

--- src/erlbin_table.erl
+++ src/erlbin_table.erl
@@ -3,61 +3,30 @@
 -behaviour(gen_server).
 
 -define(TABLE, ?MODULE).
+-define(SUBSCRIBERS, paste_subscribers).
 
--export([set/1, set/2]).
--export([exists/1]).
--export([get/1]).
--export([delete/1]).
--export([get_all/0]).
+-export([set/1,
+         set/2,
+         exists/1,
+         get/1,
+         delete/1,
+         get_all/0,
 
--export([subscribe/0]).
--export([unsubscribe/0]).
+         subscribe/0,
+         unsubscribe/0,
 
--export([init/1]).
--export([start_link/0]).
--export([handle_call/3]).
--export([handle_cast/2]).
--export([handle_info/2]).
--export([code_change/3]).
--export([terminate/2]).
+         init/1,
+         start_link/0,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         code_change/3,
+         terminate/2]).
 
 start_link() -> gen_server:start_link({local, ?TABLE}, ?MODULE, [], []).
 
-%%%%% gen_server callbacks
-
-init(_) ->
-    ets:new(?TABLE, [set, named_table, public]),
-    {ok, #{subscribers => []}}.
-
-handle_call(_Req, _From, State) ->
-    {noreply, State}.
-
-%% add a Pid to the subscribers list
-handle_cast({subscribe, Pid}, State=#{subscribers := Subs}) ->
-    NewState = State#{subscribers := [Pid | Subs]},
-    {noreply, NewState};
-
-%% remove a Pid from the subscribers list
-handle_cast({unsubscribe, Pid}, State=#{subscribers := Subs}) ->
-    NewState = State#{subscribers := lists:delete(Pid, Subs)},
-    {noreply, NewState};
-
-%% publish an table action to all subscribers
-handle_cast({publish, Action, Data}, State=#{subscribers := Subs}) ->
-    [Pid ! {table_action, Action, Data} || Pid <- Subs],
-    {noreply, State};
-
-handle_cast(_Req, State) ->
-    {noreply, State}.
-
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-code_change(_OldVsn, State, _Extra) -> {ok, State}.
-
-terminate(normal, _State) -> ok.
-
 %%%%% table API
+
 set(Data) ->
     Id = erlang:unique_integer([positive]),
     Term = {Id, Data},
@@ -92,14 +61,36 @@ delete(Id) ->
 get_all() ->
     [Data#{id => Id} || {Id, Data} <- ets:tab2list(?TABLE)]. %% I know, I know
 
-%% pub sub API
+%%%% pubsub API
 
 subscribe() ->
-    gen_server:cast(?TABLE, {subscribe, self()}).
+    ets:insert(?SUBSCRIBERS, {self()}).
 
 unsubscribe() ->
-    gen_server:cast(?TABLE, {unsubscribe, self()}).
+    ets:delete(?SUBSCRIBERS, self()).
 
 %% used internally by the functions that change the table
 publish(Action, Data) ->
-    gen_server:cast(?TABLE, {publish, Action, Data}).
+    %% maybe better to do it in a different process to avoid timeouts on the caller
+    ets:foldl(fun ({Pid}, _Acc) -> Pid ! {table_action, Action, Data} end,
+              acc0, ?SUBSCRIBERS).
+
+%%%%% gen_server callbacks
+
+init(_) ->
+    ets:new(?TABLE, [set, named_table, public]),
+    ets:new(?SUBSCRIBERS, [set, named_table, public]),
+    {ok, #{subscribers => []}}.
+
+handle_call(_Req, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Req, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+terminate(normal, _State) -> ok.
